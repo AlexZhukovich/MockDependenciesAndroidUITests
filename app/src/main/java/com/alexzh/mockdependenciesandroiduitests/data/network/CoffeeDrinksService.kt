a192fcb3ae5d344885df3c034a5ca79c13af6e85
Alex Zhukovich <zhukovich.a@gmail.com>
2020-04-02 19:03:10 +0200

    Updated version of mockk library; Updated CoffeeDrinksRepository and CoffeeDrinksService classes

--- app/src/main/java/com/alexzh/mockdependenciesandroiduitests/data/network/CoffeeDrinksService.kt
+++ app/src/main/java/com/alexzh/mockdependenciesandroiduitests/data/network/CoffeeDrinksService.kt
@@ -2,9 +2,11 @@ package com.alexzh.mockdependenciesandroiduitests.data.network
 
 import com.alexzh.mockdependenciesandroiduitests.data.model.Drink
 import retrofit2.http.GET
+import java.net.UnknownHostException
 
 interface CoffeeDrinksService {
 
+    @Throws(UnknownHostException::class)
     @GET("coffee-drinks.json")
     suspend fun getCoffeeDrinks(): List<Drink>
 }
